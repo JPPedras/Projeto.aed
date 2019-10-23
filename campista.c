bf238f034dfd9091a6598e2f5b1e3e61d399fa4a
MEEC PROG 1718 <joaoliveirapedras@gmail.com>
2019-10-23 17:06:42 +0100

    asdf

--- campista.c
+++ campista.c
@@ -1,8 +1,35 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 #include"print.h"
 
-int main(){
+typedef struct{
 
-    printoi();
+  int L;
+  int C;
+  int *lines;
+  int *columns;
+  char **map;
+
+}pb;
+
+int main(int argc, char *argv[]){
+
+  FILE *fp;
+  pb *prob;
+
+  prob=(pb*)malloc(sizeof(pb));
+
+  fp=fopen(argv[1],"r");
+
+  do{
+    
+
+  }while(feof(fp)!=NULL);
+
+
+  free(prob);
+
+
   return 0;
 }
--- print.h
+++ print.h
@@ -1,10 +1,3 @@
-#ifndef _PRINT_H
-#define _PRINT_H
 
-#include<stdio.h>
 
 void printoi();
-
-
-
-#endif
