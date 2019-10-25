162f92ef9390a6149e75a89ca2db1dab133903e5
MEEC PROG 1718 <goncalocoelho2003@gmail.com>
2019-10-25 15:12:25 +0100

    fisji

--- analysis.c
+++ analysis.c
@@ -22,7 +22,7 @@ int check_a(pb *prob){
     }
   }
 
-  if(tent_count[0]!=tent_count[1] || tree_count<tent_count[0] || tree_count<tent_count[1])
+  if(tent_count[0]!=tent_count[1] || tree_count<tent_count[0])
     return(0);
 
   return 1;
@@ -30,23 +30,47 @@ int check_a(pb *prob){
 
 int check_b(pb *prob){
 
-  int tree_count=0,i,j=1,*diret;
+  int tree_count=0,i,*tent_count,*a_sides,cond2=0;
 
-  diret=(int*)malloc(4*sizeof(int));
+  a_sides=(int*)malloc(4*sizeof(int));
+  tent_count=(int*)malloc(2*sizeof(int));
+
+  for(i=0;i<4;i++){
+    a_sides[i]=0;
+  }
 
   if(prob->cd[0]==0)
-    diret[0]=-1;
+    a_sides[0]=-1;
   if(prob->cd[0]==(prob->L)-1)
-    down=-1
+    a_sides[1]=-1;
+  if(prob->cd[1]==0)
+    a_sides[2]=-1;
+  if(prob->cd[1]==(prob->C)-1)
+    a_sides[3]=-1;
 
+  if(a_sides[0]==0)
+    if(prob->map[(prob->cd[0])-1][(prob->cd[1])]=='A')
+      tree_count++;
+  if(a_sides[1]==0)
+    if(prob->map[(prob->cd[0])+1][(prob->cd[1])]=='A')
+      tree_count++;
+  if(a_sides[2]==0)
+    if(prob->map[(prob->cd[0])][(prob->cd[1])-1]=='A')
+      tree_count++;
+  if(a_sides[3]==0)
+    if(prob->map[(prob->cd[0])-1][(prob->cd[1]+1)]=='A')
+      tree_count++;
 
-  for(i=0;i<2;i++){
-
-    if((prob->map[(prob->cd[0])+i][(prob->cd[1])-j]=='A') || (prob->map[(prob->cd[0])-j][(prob->cd[1])+i]=='A')) {
-        tree_count++;
-    }
-    j=0;
+  if(tree_count==0){
+    return(1);
   }
+
+  for(i=0;i<prob->l;i++){
+    if(prob->map[i][cd[]])
+  }
+
+  free(a_sides);
+  free(tent_count);
 
   printf("trees:%d\n",tree_count);
   return(1);
